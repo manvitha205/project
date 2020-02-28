d666b20616a5d296d842194c964944ce64362ec1
geedi <geedi@naresh-PC>
2020-02-28 16:26:09 +0530

    manu

---
+++ First project/src/firstpackage/Conditions.java
@@ -0,0 +1,21 @@
+package firstpackage;
+
+public class Conditions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=20;
+		int j=30;
+		if(i>j)
+		{
+			System.out.println("i is not greater");}
+			else{
+				System.out.println("j is greater");
+			}
+			
+			
+			
+				
+			}
+}
